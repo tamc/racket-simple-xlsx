72a0d8595b8c3456b4c74663185931239f63422b
Chen Xiao <chenxiao770117@gmail.com>
2018-08-28 09:34:58 +0800

    update

--- simple-xlsx/writer/xl/styles-test/styles-color-test.rkt
+++ simple-xlsx/writer/xl/styles-test/styles-color-test.rkt
@@ -9,9 +9,9 @@
 (define (test-data) @S{
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 
-  <styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
 
-  <fonts count="2">
+  <fonts count="1">
     <font>
       <sz val="11"/>
       <color theme="1"/>
@@ -20,41 +20,35 @@
       <charset val="134"/>
       <scheme val="minor"/>
     </font>
-    
-    <font>
-      <sz val="9"/>
-      <name val="宋体"/>
-      <family val="2"/>
-      <charset val="134"/>
-      <scheme val="minor"/>
-    </font>
-    
-    </fonts>
+  </fonts>
 
-    <fills count="5">
-      <fill><patternFill patternType="none"/></fill>
-      <fill><patternFill patternType="gray125"/></fill>
-      <fill><patternFill patternType="solid"><fgColor rgb="FF0000"/><bgColor indexed="64"/></patternFill></fill>
-      <fill><patternFill patternType="solid"><fgColor rgb="00FF00"/><bgColor indexed="64"/></patternFill></fill>
-      <fill><patternFill patternType="solid"><fgColor rgb="0000FF"/><bgColor indexed="64"/></patternFill></fill>
-    </fills>
+  <fills count="4">
+    <fill><patternFill patternType="none"/></fill>
+    <fill><patternFill patternType="solid"><fgColor rgb="FF0000"/><bgColor indexed="64"/></patternFill></fill>
+    <fill><patternFill patternType="solid"><fgColor rgb="00FF00"/><bgColor indexed="64"/></patternFill></fill>
+    <fill><patternFill patternType="solid"><fgColor rgb="0000FF"/><bgColor indexed="64"/></patternFill></fill>
+  </fills>
     
-    <borders count="1">
-      <border><left/><right/><top/><bottom/><diagonal/></border>
-    </borders>
+  <borders count="1">
+    <border><left/><right/><top/><bottom/><diagonal/></border>
+  </borders>
     
-    <cellStyleXfs count="1">
-      <xf numFmtId="0" fontId="0" fillId="0" borderId="0"><alignment vertical="center"/></xf>
-    </cellStyleXfs>
+  <cellStyleXfs count="1">
+    <xf numFmtId="0" fontId="0" fillId="0" borderId="0"><alignment vertical="center"/></xf>
+  </cellStyleXfs>
 
-    <cellXfs count="4">
-      <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"><alignment vertical="center"/></xf>
-      <xf numFmtId="0" fontId="0" fillId="2" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
-      <xf numFmtId="0" fontId="0" fillId="3" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
-      <xf numFmtId="0" fontId="0" fillId="4" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
-    </cellXfs>
+  <cellXfs count="4">
+    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"><alignment vertical="center"/></xf>
+    <xf numFmtId="0" fontId="0" fillId="1" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
+    <xf numFmtId="0" fontId="0" fillId="2" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
+    <xf numFmtId="0" fontId="0" fillId="3" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
+  </cellXfs>
     
-    <cellStyles count="1"><cellStyle name="常规" xfId="0" builtinId="0"/></cellStyles><dxfs count="0"/><tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleLight16"/></styleSheet>
+  <cellStyles count="1"><cellStyle name="常规" xfId="0" builtinId="0"/></cellStyles>
+
+  <dxfs count="0"/><tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleLight16"/>
+
+</styleSheet>
 })
 
 (define test-styles
--- simple-xlsx/writer/xl/styles.rkt
+++ simple-xlsx/writer/xl/styles.rkt
@@ -12,11 +12,101 @@
 (provide (contract-out
           [write-styles (-> list? string?)]
           [write-styles-file (-> path-string? (is-a?/c xlsx%) void?)]
+          [write-header (-> string?)]
+          [write-fonts (-> string?)]
+          [write-fills (-> string?)]
+          [write-borders (-> string?)]
+          [write-cellStyleXfs (-> string?)]
+          [write-cellXfs (-> string?)]
+          [write-cellStyles (-> string?)]
+          [write-dxfs (-> string?)]
+          [write-footer (-> string?)]
           ))
 
 (define S string-append)
 
+(define (write-header) @S{
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+
+<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+})
+
+(define (write-fonts) @S{
+  <fonts count="1">
+    <font>
+      <sz val="11"/>
+      <color theme="1"/>
+      <name val="宋体"/>
+      <family val="2"/>
+      <charset val="134"/>
+      <scheme val="minor"/>
+    </font>
+  </fonts>
+})
+
+(define (write-fills) @S{
+  <fills count="4">
+    <fill><patternFill patternType="none"/></fill>
+    <fill><patternFill patternType="solid"><fgColor rgb="FF0000"/><bgColor indexed="64"/></patternFill></fill>
+    <fill><patternFill patternType="solid"><fgColor rgb="00FF00"/><bgColor indexed="64"/></patternFill></fill>
+    <fill><patternFill patternType="solid"><fgColor rgb="0000FF"/><bgColor indexed="64"/></patternFill></fill>
+  </fills>
+})
+
+(define (write-borders) @S{
+  <borders count="1">
+    <border><left/><right/><top/><bottom/><diagonal/></border>
+  </borders>
+})
+
+(define (write-cellStyleXfs) @S{
+  <cellStyleXfs count="1">
+    <xf numFmtId="0" fontId="0" fillId="0" borderId="0"><alignment vertical="center"/></xf>
+  </cellStyleXfs>
+})
+
+(define (write-cellXfs) @S{
+  <cellXfs count="4">
+    <xf numFmtId="0" fontId="0" fillId="0" borderId="0" xfId="0"><alignment vertical="center"/></xf>
+    <xf numFmtId="0" fontId="0" fillId="1" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
+    <xf numFmtId="0" fontId="0" fillId="2" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
+    <xf numFmtId="0" fontId="0" fillId="3" borderId="0" xfId="0" applyFill="1"><alignment vertical="center"/></xf>
+  </cellXfs>
+})
+
+(define (write-cellStyles) @S{
+  <cellStyles count="1"><cellStyle name="常规" xfId="0" builtinId="0"/></cellStyles>
+})
+
+(define (write-dxfs) @S{
+  <dxfs count="0"/><tableStyles count="0" defaultTableStyle="TableStyleMedium9" defaultPivotStyle="PivotStyleLight16"/>
+})
+
+(define (write-footer) @S{
+</styleSheet>
+})
+
 (define (write-styles color_list) @S{
+@|(write-header)|
+
+@|(write-fonts)|
+
+@|(write-fills)|
+
+@|(write-borders)|
+
+@|(write-cellStyleXfs)|
+
+@|(write-cellXfs)|
+
+@|(write-cellStyles)|
+
+@|(write-dxfs)|
+
+@|(write-footer)|
+})
+
+(define (write-styles-bak color_list) @S{
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"><fonts count="2"><font><sz val="11"/><color theme="1"/><name val="宋体"/><family val="2"/><charset val="134"/><scheme val="minor"/></font><font><sz val="9"/><name val="宋体"/><family val="2"/><charset val="134"/><scheme val="minor"/></font></fonts><fills count="@|(number->string (+ 2 (length color_list)))|"><fill><patternFill patternType="none"/></fill><fill><patternFill patternType="gray125"/></fill>@|(with-output-to-string
   (lambda ()
